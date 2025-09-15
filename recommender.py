01ffcf9b2420f3c43cdc49ea253f97696b9dfa15
Mouheb Mahfoudhi <mouhebmahfoudhi8@gmail.com>
2025-09-15 21:11:04 +0100

    recommender.py return a list of dictionaries containing the title, score and sypnosis of recommended animes

--- recommender.py
+++ recommender.py
@@ -1,12 +1,29 @@
 import identifier
 import requests
 
-def recommender():
-    recommendation_limit = 0
-    recommendations=[]
-    anime_id = identifier.return_anime_id()
+def recommended_animes(anime_id):
+    recommended_list = []
     r = requests.get(f"https://api.jikan.moe/v4/anime/{anime_id}/recommendations")
     recommended = r.json()
     for entry in recommended['data'][:10]:
-        recommendations.append(entry['entry']['title'])
+        recommended_list.append(entry['entry']['mal_id'])
+
+    return recommended_list
+
+
+def recommender():
+    recommendations=[]
+    anime_id = identifier.return_anime_id()
+    id_list = recommended_animes(anime_id)
+    for id in id_list:
+        r = requests.get(f"https://api.jikan.moe/v4/anime/{id}")
+        anime = r.json()
+        if "data" in anime:
+            tmp_dict = {"title": anime['data']['titles'][0]['title'], "score": anime['data']['score'], "synopsis" : anime['data']['synopsis'] }
+            print(tmp_dict)
+            recommendations.append(tmp_dict)
+        else:
+            pass
     return recommendations
+
+
