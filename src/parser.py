8ff694ccaa4f2ddc451bacbdfe1bb6dfcbb4a90b
Mouheb Mahfoudhi <mouhebmahfoudhi8@gmail.com>
2025-09-17 23:28:57 +0100

    started parsing

--- src/parser.py
+++ src/parser.py
@@ -1,5 +1,24 @@
 import requests
+from bs4 import BeautifulSoup
 import utils
 
-def get_info_anime(title):
-    r = requests.get(f"https://myanimelist.net/anime/{utils.return_anime_id(title)}")
+def get_info(title):
+
+    if(utils.selection_choice() in ["A","ANIME"]):
+        r = requests.get(f"https://myanimelist.net/anime/{utils.return_anime_id(title)}")
+
+        soup = BeautifulSoup(r.text,'lxml')
+        
+        episodes_label = soup.find("span", string="Episodes:")
+        episodes = episodes_label.next_sibling.strip()
+
+        status_label = soup.find("span", string="Status:")
+        status = status_label.next_sibling.strip()
+
+        ratingValue_label = soup.find("span", string="ratingValue:")
+        ratingValue = ratingValue_label.next_sibling.strip()
+        
+        print("Episodes:", episodes)
+        print("Status:", status)
+        
+get_info('monster')
--- src/utils.py
+++ src/utils.py
@@ -6,3 +6,16 @@ def return_anime_id(anime_title):
     data = r.json()
     first_result = data["data"][0]
     return first_result["mal_id"]
+
+def return_manga_id(manga_title):
+    
+    r = requests.get(f"https://api.jikan.moe/v4/manga?q={manga_title.replace(' ', '%20')}")
+    data = r.json()
+    first_result = data["data"][0]
+    return first_result["mal_id"]
+
+def selection_choice():
+    choice = input("are you looking for a (M)anga or an (A)nime? \n")
+    return choice.upper()
+
+    
